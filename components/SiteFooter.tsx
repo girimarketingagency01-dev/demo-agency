6e5d01a3678b7ceca7482f08e9efde0291b81237
Aniket <girimarketingagency01@gmail.com>
2026-09-17 18:58:59 +0530

    Update website

--- components/SiteFooter.tsx
+++ components/SiteFooter.tsx
@@ -188,7 +188,7 @@ export default function SiteFooter() {
           </span>
 
           <span className="footer-infinity-line">
-  Made with <span className="footer-heart">♥</span> in India · By Mehak Bhatnagar
+  Made with <span className="footer-heart">♥</span> in India · By Aniket
 </span>
 
         </div>
